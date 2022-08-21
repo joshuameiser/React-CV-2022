25c365c8a6e933091ef756587fbd5080a7cc9fbe
Joshua Meiser <jm@joshuameiser.de>
2022-08-21 13:36:34 +0200

    fixed useEffect for closing other categories and put the categories and text sections in their own components

--- src/pages/AboutMe/AboutMe.tsx
+++ src/pages/AboutMe/AboutMe.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeartbeat } from "@fortawesome/free-solid-svg-icons";
+import { Category } from "./components/Category";
 
 const Wrapper = styled.div`
 	height: 100vh;
@@ -23,126 +24,12 @@ const Headline = styled.h1<{ color: string }>`
 	color: ${(p) => p.color};
 `;
 
-const Description = styled(motion.p)<{ inView: boolean }>`
-	width: 40%;
-	height: 60px;
-	font-size: 3rem;
-	color: white;
-	display: flex;
-	justify-content: left;
-	margin: 16px 0;
-	margin-left: 4rem;
-	transition: color 0.3s ease-in-out;
-	align-items: center;
-	color: inherit;
-	user-select: none;
-`;
-
-// This does not work. Instead just have the + which changes to an -, but do it with transitions
-const SVG = styled(motion.svg)<{ inView: boolean; clicked: boolean }>`
-	height: 20px;
-	width: 20px;
-	cursor: pointer;
-	fill: inherit;
-	overflow: visible;
-	& path {
-		fill: inherit;
-		cursor: pointer;
-	}
-`;
-
-const DescriptionOpener = (props: { inView: boolean; clicked: boolean }) => (
-	<SVG
-		clicked={props.clicked}
-		inView={props.inView}
-		xmlns="http://www.w3.org/2000/svg"
-		animate={props.inView ? { x: 0 } : { x: "100vw" }}
-		transition={{ duration: 1 }}
-		viewBox="0 0 20 2">
-		<motion.path
-			animate={props.clicked ? { rotate: 180 } : { rotate: 0 }}
-			className="plus-line"
-			d="M0 0H20V2H0z"></motion.path>
-		<motion.path
-			animate={props.clicked ? { rotate: 360 } : { rotate: 90 }}
-			d="M0 0H20V2H0z"></motion.path>
-	</SVG>
-);
-
-const DescriptionWrapper = styled.div<{ inView: boolean; clicked: boolean }>`
-	display: flex;
-	justify-content: flex-start;
-	align-items: center;
-	cursor: pointer;
-	fill: ${(p) =>
-		p.clicked
-			? "var(--primaryColor)"
-			: p.inView
-			? "var(--secondaryColor)"
-			: "var(--contrastColor)"};
-	color: ${(p) =>
-		p.clicked
-			? "var(--primaryColor)"
-			: p.inView
-			? "var(--secondaryColor)"
-			: "var(--contrastColor)"};
-	&:hover {
-		color: var(--primaryColor);
-		path {
-			fill: var(--primaryColor);
-		}
-	}
-`;
-
-const TextSection = styled(motion.div)<{}>`
-	max-width: 600px;
-	color: white;
-	font-size: 1.5rem;
-	padding: 2rem;
-	display: flex;
-	flex-direction: column;
-	align-items: flex-start;
-	overflow: hidden;
-	margin: 0 4rem;
-	border-radius: 16px;
-
-	p {
-		padding: 0;
-		margin: 0;
-		text-align: left;
-		line-height: 2rem;
-	}
-`;
-
 const IconWrapper = styled(motion.div)`
 	height: 20px;
 	width: 20px;
 	display: inline-block;
 	margin-left: 1rem;
 `;
-
-const Category = (props: {
-	inView: boolean;
-	children: React.ReactNode;
-	clicked: boolean;
-	onClick: Function;
-	icon?: React.ReactNode;
-}) => {
-	return (
-		<DescriptionWrapper
-			clicked={props.clicked}
-			inView={props.inView}
-			onClick={(e) => props.onClick(e)}>
-			<Description
-				animate={props.inView ? { y: 0 } : { y: "100vw" }}
-				transition={{ duration: 1 }}
-				inView={props.inView}>
-				{props.children}
-			</Description>
-			<DescriptionOpener inView={props.inView} clicked={props.clicked} />
-		</DescriptionWrapper>
-	);
-};
 
 export const AboutMe = () => {
 	const [color, setColor] = useState<string>("aqua");
@@ -152,6 +39,7 @@ export const AboutMe = () => {
 
 	const [clickedOne, setClickedOne] = useState<boolean>(false);
 	const [clickedTwo, setClickedTwo] = useState<boolean>(false);
+	const [clickedThree, setClickedThree] = useState<boolean>(false);
 
 	useEffect(() => {
 		setClickedOne(false);
@@ -162,12 +50,23 @@ export const AboutMe = () => {
 	}, [twoInView]);
 
 	useEffect(() => {
-		clickedOne && clickedTwo && setClickedTwo(false);
+		setClickedThree(false);
+	}, [threeInView]);
+
+	useEffect(() => {
+		clickedTwo && clickedOne && setClickedTwo(false);
+		clickedThree && clickedOne && setClickedThree(false);
 	}, [clickedOne]);
 
 	useEffect(() => {
 		clickedOne && clickedTwo && setClickedOne(false);
+		clickedThree && clickedTwo && setClickedThree(false);
 	}, [clickedTwo]);
+
+	useEffect(() => {
+		clickedOne && clickedThree && setClickedOne(false);
+		clickedTwo && clickedThree && setClickedTwo(false);
+	}, [clickedThree]);
 
 	window.onscroll = function () {
 		"use strict";
@@ -209,20 +108,13 @@ export const AboutMe = () => {
 		<Wrapper>
 			<Headline color={color}>Who am I?</Headline>
 			<Category
+				title="Developer"
 				clicked={clickedOne}
 				onClick={() => {
 					setClickedOne(!clickedOne);
 					console.log("clicked");
 				}}
 				inView={oneInView}>
-				Developer
-			</Category>
-			<TextSection
-				animate={
-					clickedOne
-						? { height: "auto", border: "2px solid var(--primaryColor)" }
-						: { height: 0, padding: 0, border: "none" }
-				}>
 				<p>I am a passionate Frontend developer with a clear love for React.</p>
 				<p>
 					I love creating components, or entire websites utilizing SCSS or
@@ -237,29 +129,34 @@ export const AboutMe = () => {
 						<FontAwesomeIcon icon={faHeartbeat} />
 					</IconWrapper>
 				</p>
-			</TextSection>
+			</Category>
 			<Category
+				title="Climber"
 				clicked={clickedTwo}
 				onClick={() => {
 					setClickedTwo(!clickedTwo);
 				}}
 				inView={twoInView}>
-				Climber
-			</Category>
-			<TextSection
-				animate={
-					clickedTwo
-						? { height: "auto", border: "2px solid var(--primaryColor)" }
-						: { height: 0, padding: 0, border: "none" }
-				}>
 				{/* Would like to add more information about my bouldering here. Maybe link to another website once I have one. */}
 				<p>
 					After work, my heart primarily beats for climbing, or more
 					specifically for bouldering. I have been doing this sport since the
 					age of 13 and could not imagine a life without it anymore.
 				</p>
-			</TextSection>
-			{/* <Category inView={threeInView}>Climber</Category> */}
+			</Category>
+			<Category
+				title="Consultant at Aclue"
+				onClick={() => setClickedThree(!clickedThree)}
+				clicked={clickedThree}
+				inView={threeInView}>
+				<p>
+					I am currently employed at Aclue, working 40 hours each week as a
+					Frontend consultant. Since my start on the 01.03.2022, I have been
+					working on a project for OTTO. The project contains the aiding of the
+					creation, as well as the maintanance and documentation of a design
+					system based on MUI.
+				</p>
+			</Category>
 		</Wrapper>
 	);
 };
---
+++ src/pages/AboutMe/components/Category.tsx
@@ -0,0 +1,129 @@
+import { motion } from "framer-motion";
+import styled from "styled-components";
+
+const DescriptionWrapper = styled.div<{ inView: boolean; clicked: boolean }>`
+	display: flex;
+	justify-content: flex-start;
+	align-items: center;
+	cursor: pointer;
+	fill: ${(p) =>
+		p.clicked
+			? "var(--primaryColor)"
+			: p.inView
+			? "var(--secondaryColor)"
+			: "var(--contrastColor)"};
+	color: ${(p) =>
+		p.clicked
+			? "var(--primaryColor)"
+			: p.inView
+			? "var(--secondaryColor)"
+			: "var(--contrastColor)"};
+	&:hover {
+		color: var(--primaryColor);
+		path {
+			fill: var(--primaryColor);
+		}
+	}
+`;
+
+const CategoryWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	cursor: pointer;
+`;
+
+const SVG = styled(motion.svg)<{ inView: boolean; clicked: boolean }>`
+	height: 20px;
+	width: 20px;
+	cursor: pointer;
+	fill: inherit;
+	overflow: visible;
+	& path {
+		fill: inherit;
+		cursor: pointer;
+	}
+`;
+
+const Description = styled(motion.p)<{ inView: boolean }>`
+	width: 40%;
+	height: 60px;
+	font-size: 3rem;
+	color: white;
+	display: flex;
+	justify-content: left;
+	margin: 16px 0;
+	margin-left: 4rem;
+	transition: color 0.3s ease-in-out;
+	align-items: center;
+	color: inherit;
+	user-select: none;
+`;
+
+const TextSection = styled(motion.div)<{}>`
+	max-width: 600px;
+	color: white;
+	font-size: 1.5rem;
+	padding: 2rem;
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	overflow: hidden;
+	margin: 0 4rem;
+	border-radius: 16px;
+
+	p {
+		padding: 0;
+		margin: 0;
+		text-align: left;
+		line-height: 2rem;
+	}
+`;
+
+const DescriptionOpener = (props: { inView: boolean; clicked: boolean }) => (
+	<SVG
+		clicked={props.clicked}
+		inView={props.inView}
+		xmlns="http://www.w3.org/2000/svg"
+		animate={props.inView ? { x: 0 } : { x: "100vw" }}
+		transition={{ duration: 1 }}
+		viewBox="0 0 20 2">
+		<motion.path
+			animate={props.clicked ? { rotate: 180 } : { rotate: 0 }}
+			className="plus-line"
+			d="M0 0H20V2H0z"></motion.path>
+		<motion.path
+			animate={props.clicked ? { rotate: 360 } : { rotate: 90 }}
+			d="M0 0H20V2H0z"></motion.path>
+	</SVG>
+);
+
+export const Category = (props: {
+	inView: boolean;
+	title: string;
+	children: React.ReactNode;
+	clicked: boolean;
+	onClick: Function;
+	icon?: React.ReactNode;
+}) => {
+	return (
+		<CategoryWrapper onClick={(e) => props.onClick(e)}>
+			<DescriptionWrapper clicked={props.clicked} inView={props.inView}>
+				<Description
+					animate={props.inView ? { y: 0 } : { y: "100vw" }}
+					transition={{ duration: 1 }}
+					inView={props.inView}>
+					{props.title}
+				</Description>
+				<DescriptionOpener inView={props.inView} clicked={props.clicked} />
+			</DescriptionWrapper>
+			<TextSection
+				animate={
+					props.clicked
+						? { height: "auto", border: "2px solid var(--primaryColor)" }
+						: { height: 0, padding: 0, border: "none" }
+				}>
+				{props.children}
+			</TextSection>
+		</CategoryWrapper>
+	);
+};
